250ff4608e10ca634ae2ef233fd85a1edb0c36dd
Keviana Brailsford-Mickens <99054758+bkeviana@users.noreply.github.com>
2022-06-25 13:33:37 -0400

    updating script code

--- script.js
+++ script.js
@@ -14,72 +14,147 @@
 //WHEN the password is generated
 //THEN the password is either displayed in an alert or written to the page
 
-let generateBtn = document.querySelector("#generate");
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let lowerCase = "abcdefghijklmnopqrstuvwxyz";
-let numbers = "0123456789";
-let specialChars = "!@,#$%&*{}[]/\\+=";
-let allCharacters = "";
+//Creating password object.
+var pwdCriteria = {
 
+  //Property for length of password
+  pwdLength: 0,
 
-function writePassword(){
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+  //array to hold lowercase letters
+  pwdLowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+
+  //array to hold uppercase letters
+  pwdUpperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+
+  //array to hold numbers
+  pwdNumber: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+
+  //array to hold special characters
+  pwdCharacter: ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
+    "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]//32
+}
+
+// Write password to the #password input on index.html
+function writePassword() {
+  //call generatePassword function
+  var password = generatePassword();
+  
+  //set passwordText = to the textArea on index.html witht he ID of password
+  var passwordText = document.querySelector("#password");
+
+  //update the textArea with the new password
   passwordText.value = password;
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-generateBtn.addEventListener("click", writePassword)
+//function to handle the operations to generate a new password
+function generatePassword() {
 
-function generatePassword(){
- 
-  let result = "";
-  
-  let length = prompt("How many characters do you want to have in your secured password?  choose between 8 and 128");
-  if(isNaN(length)){
-    alert("You must type in a number to be valid! Try Again!");
-    return generatePassword()
+  //holds the password to be generated and returned 
+  var result = "";
+
+  //variables to collect input from user prompts
+  var passwordLength = 0;
+  var upperCase;
+  var lowerCase;
+  var numbers;
+  var specialChar;
+
+  //initialize characters
+  passwordLength = 0;
+  pwdCriteria.pwdLength = 0;
+  result = "";
+
+  //check password length
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("How many characters do you want your password to be? \nPassword must be between 8 and 128 characters.");
+
+    //if user presses cancel
+    if (passwordLength === null) {
+      return "Your secure password";
+    }
+    else {
+      //checking user enters an integer
+      if (!isFinite(passwordLength)) {
+        alert("You did not enter a number");
+        return "Your secure password";
+      }
+      else {
+        //check password meets length criteria
+        if (passwordLength < 8 || passwordLength > 128) {
+          alert("Password must be between 8 and 128 characters.");
+          return "Your secure password";
+        }
+        else {
+
+          //call the internal function to show prompts for criteria
+          showPrompts();
+
+          //keep adding characters based on criteria until pwdLength is = to the length the user set
+          while (pwdCriteria.pwdLength < passwordLength) {
+            //if statement to make sure the user selects at least one of the criteria  
+            if (lowerCase === false && upperCase === false && numbers === false && specialChar === false) {
+              alert("You must select at least one criteria of lowercase, uppercase, numbers or special characters")
+              showPrompts();
+            }
+            else {
+              //if the user selected lowercase and there is still room to add characters then
+              //randomly grab a lowercase letter from the array and add it to the end of result 
+              //update pwdLength by 1
+              if (lowerCase === true && pwdCriteria.pwdLength < passwordLength) {
+                var lc = pwdCriteria.pwdLowerCase[Math.floor(Math.random() * 26)]
+                result = result + lc;
+                pwdCriteria.pwdLength++;
+              }
+
+              //if the user selected a special character and there is still room to add characters then
+              //randomly grab a apecial character from the array and add it to the end of result 
+              //update pwdLength by 1              
+              if (specialChar === true && pwdCriteria.pwdLength < passwordLength) {
+                var sc = pwdCriteria.pwdCharacter[Math.floor(Math.random() * 32)]
+                result = result + sc;
+                pwdCriteria.pwdLength++;
+              }
+
+              //if the user selected an uppercase letter and there is still room to add characters then
+              //randomly grab an uppercase letter from the array and add it to the end of result 
+              //update pwdLength by 1
+              if (upperCase === true && pwdCriteria.pwdLength < passwordLength) {
+                var uc = pwdCriteria.pwdUpperCase[Math.floor(Math.random() * 26)]
+                result = result + uc;
+                pwdCriteria.pwdLength++;
+              }
+
+              //if the user selected a number and there is still room to add characters then
+              //randomly grab a number from the array and add it to the end of result 
+              //update pwdLength by 1
+              if (numbers === true && pwdCriteria.pwdLength < passwordLength) {
+                var num = pwdCriteria.pwdNumber[Math.floor(Math.random() * 10)]
+                result = result + num;
+                pwdCriteria.pwdLength++;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    //return the generated password back to the calling function
+    return result;
+
+    //internal function to prompt the user for criteria
+    function showPrompts() {
+      lowerCase = confirm("Do you want to use lower case letters?");
+      upperCase = confirm("Do you want to use upper case letters?");
+      numbers = confirm("Do you want to use numbers?");
+      specialChar = confirm("Do you want to use any special characters?");
+    }
   }
-  if(length<8 || length> 128){
-    alert("Sorry, Try Again. Please choose a number between 8 - 128!");
-    return generatePassword()
-  }
- 
-  let hasUpperCase = confirm("Do you want to include any upper case characters?");
-  let hasLowerCase = confirm("Do you want to include any lower case characters?");
-  let hasNumbers = confirm("Do you want to include any numbers?");
-  let hasSpecialChars = confirm("Do you want to include any special characters? REQUIRED!");
-
-  if(!hasUpperCase&&!hasLowerCase&&!hasNumbers&&!hasSpecialChars){
-  return generatePassword()
 }
-else { 
-    alert("You must have at least one character type, Try Again!")
-  }
-
-if(hasUpperCase){
-    allCharacters += upperCase
-  }
-  if(hasLowerCase){
-    allCharacters += lowerCase
-  }
-
-  if(hasNumbers) {
-    allCharacters += numbers
-  }
-
-  if(hasSpecialChars) {
-    allCharacters += specialChars
-  }
-
-for (let i = 0; i < length; i++) {
-  result += allCharacters.charAt(Math.floor(Math.random() * allCharacters.length));
-}
-return result;
-
-}
-
-
-
-
